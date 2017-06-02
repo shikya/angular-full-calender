064e993bb0fb1325b52caf8273dda7e310dc3ab2
Shrikant Sonone <shrikant.sonone@coda.global>
2017-06-02 11:26:42 +0530

    Simplified the displayes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,50 @@ export class AppComponent {
   ngOnInit(){
 		$('#calendar').fullCalendar({
 			header: {
-				left: 'prev,next today',
-				center: 'title',
+				left: '',
+				center: 'prev,title,next',
 				right: 'month,basicWeek,basicDay'
 			},
 			dayNames:['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
 			navLinks: true, // can click day/week names to navigate views
 			editable: true,
-			eventLimit: true, // allow "more" link when too many events
+			eventLimit: 1, // allow "more" link when too many events
 			events: [
 				{
 					title: 'All Day Event',
 					start: '2017-05-01'
 				},
 				{
-					title: 'Long Event',
-					start: '2017-05-07',
-					end: '2017-05-10'
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
+				},
+				{
+					title: 'All Day Event',
+					start: '2017-05-01'
 				}
 			]
 		});    
